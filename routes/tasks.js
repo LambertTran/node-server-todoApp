b80892a9721332ed7ad0138964a7e182bf626635
lam tran <lb.tran648@gmail.com>
2017-08-24 13:11:51 -0700

    add PUT route

--- routes/tasks.js
+++ routes/tasks.js
@@ -28,16 +28,18 @@ router.get('/tasks', authentication,(req, res) => {
 
 /** return an task using id from user input */
 // router.get('/tasks/:id', authentication, (req, res) => {
-//   Task.findOne({
-//     _id: req.params.id
-//   }).then( (task) =>{
-//       task.aggregate(
-//       [
-//         {$group: {$sum:["expect","finish"] }}
-//       ]
-//       );
-//       res.send(task);
-//     })
+//   // Task.findOne({
+//   //   _id: req.params.id
+//   // }).then((task) =>{
+//   //     task.aggregate(
+//   //     [
+//   //       {$group: {$divide:["$expect","$finish"] }}
+//   //     ]
+//   //     );
+//   //     res.send(task);
+//   //   })
+
+//   db.
 // });
 
 
@@ -60,57 +62,45 @@ router.post('/tasks',authentication, (req, res) => {
 
 
 /** delete a single task with corresponding id */
-router.delete('/tasks/:id', (req, res, next) => {
+router.delete('/tasks/:id',authentication,(req, res) => {
   Task.remove({_id:req.params.id}).then((task) =>{
     res.status(200).send({
       _id:req.params.id
     });
   }, (err) => {
     res.status(400).send(task);
-  })
+  });
 });
 
 
 /** delete all task*/
-router.delete('/tasks&delete=all',authentication,(req,res) => {
-  Task.remove({
-    creator:req.user._id
-  })
-  .then((task) => {
-    res.status(200).send(task);
-  },(err) => {
-    res.status(400).send(err);
-  })
-  
-});
-
-// // update tasks
-// router.put('/task/:id', (req, res, next) => {
-// 	var task = req.body;
-// 	var updateTask = {};
-
-// 	if (task.status){
-// 		updateTask.status = task.status;
-// 	}
-
-// 	if (task.title){
-// 		updateTask.title = task.title;
-// 	}
-
-// 	if(!updateTask){
-// 		res.status(400).json({
-// 			"error":"bad data"
-// 		});
-// 	} else {
-// 		db.tasks.update({_id: mongojs.ObjectId(req.params.id)},updateTask,{},(err,task) =>{
-// 			if(err) {
-// 				res.send(err);
-// 			}
-// 			res.json(task);
-// 		});
-// 	}
-
+// router.delete('/tasks&delete=all',authentication,(req,res) => {
+//   Task.remove({
+//     creator:req.user._id
+//   })
+//   .then((task) => {
+//     res.status(200).send(task);
+//   },(err) => {
+//     res.status(400).send(err);
+//   })
 // });
+
+
+/** update tasks */
+router.put('/tasks/:id', authentication,(req, res) => {
+  Task.findByIdAndUpdate({_id:req.params.id},
+  {
+    $set:{ complete:true}
+  }
+  ,{new:true}, function(err,task) {
+    if (err) {
+      res.status(400).send(err)
+    }
+    else {
+      res.status(200).send(task);
+    }
+  });
+});
 
 
 module.exports = router;
